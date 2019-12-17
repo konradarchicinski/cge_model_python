fdbebdfd8bf773275a8291d5042b87120cdcd70a
gchadysz <g.chadysz@student.uw.edu.pl>
2019-12-17 19:11:50 +0100

    Exported smaller matrices to csv files; added such possibility to the code

--- Code/Sam_Partition.py
+++ Code/Sam_Partition.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-macro = pd.read_excel("C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/SAM_Macro.xlsx",
+macro = pd.read_excel('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/SAM_Macro.xlsx',
                       header = 0)
 macro = macro.drop(axis = 0, index = 0) #cleaning excel error
 
-micro = pd.read_excel("C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/SAM_Micro.xlsx",
+micro = pd.read_excel('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/SAM_Micro.xlsx',
                       header = 0, index_col = 0)
 
 def extract(df, r = None, c = None):
@@ -26,17 +26,21 @@ households = ['hhd-0', 'hhd-1', 'hhd-2', 'hhd-3', 'hhd-4', 'hhd-5', 'hhd-6', 'hh
               'hhd-91', 'hhd-92', 'hhd-93', 'hhd-94', 'hhd-95', 'gov']
 factors = ['flab-p', 'flab-m', 'flab-s', 'flab-t', 'fcap']
 endowment = extract(micro, households, factors)
+endowment.to_csv('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/endowment.csv', na_rep = '-')
 
 # PRODUCTION
 column_names = list(micro.columns)
 goods_p = column_names[1:62]
 production = extract(micro, factors, goods_p)
+production.to_csv('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/production.csv', na_rep = '-')
 
 # CONSUMPTION
 column_names = list(micro.columns)
 goods_c = column_names[62:166]
 consumption = extract(micro, goods_c, households)
+consumption.to_csv('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/consumption.csv', na_rep = '-')
 
 # TAX
 taxes = ['atax', 'dtax', 'mtax', 'stax']
 tax = extract(micro, taxes)
+tax.to_csv('C:/Users/Grzesiek/Desktop/Data Science/Python2019/Project/cge_model_python/Data/tax.csv', na_rep = '-')
